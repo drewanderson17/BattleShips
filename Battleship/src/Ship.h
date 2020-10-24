6d9c5365e5f9ed4c13e1c72a03ab8d8e21994df7
Danielle Mott <danielle.mott123@gmail.com>
2020-10-24 14:13:17 -0400

    Remove old versions of master

--- Battleship/src/Ship.h
+++ Battleship/src/Ship.h
@@ -22,19 +22,17 @@ private:
 
 class Ship {
 public:
-    Ship(string name, int length);
-    Ship(string name, int length, int row, int col, bool dir);
+    Ship(int length);
+    Ship(int length, int row, int col, bool dir);
     void placeShip(int row, int col, bool dir);
-    vector<vector<int>> getPos() const;
-    string toStr();
+    vector<vector<int> > getPos() const;
     int getHealth() const;
     bool hit(int row, int col); //the integer indicates how far the shot was from the head of the ship
 
 private:
     //bool dir;   // stores the direction of the ship 0:columnwise(left), 1:rowwise(down)
-    string name;
     int length;
-    vector<vector<int>> pos;//stores the (row, column, health) of each segment
+    vector<vector<int> > pos;//stores the (row, column, health) of each segment
 
 };
 
