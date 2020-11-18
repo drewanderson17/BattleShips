147c845821565bbda721a8979b5fdc23b47d969c
Eric Leuty <16eal@caslab.queensu.ca>
2020-11-18 18:32:06 -0500

    Adding files from my build to gitignore.

---
+++ FrontEndMain/FrontEndMain/menupage.cpp
@@ -0,0 +1,14 @@
+#include "menupage.h"
+#include "ui_menupage.h"
+
+MenuPage::MenuPage(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::MenuPage)
+{
+    ui->setupUi(this);
+}
+
+MenuPage::~MenuPage()
+{
+    delete ui;
+}
